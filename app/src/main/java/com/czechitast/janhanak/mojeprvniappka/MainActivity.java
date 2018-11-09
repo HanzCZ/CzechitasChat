7617be7e95343fd62c94ce52be3758c2c2b6b963
janhanak <honzales@gmail.com>
2018-11-09 16:49:43 +0100

    bez MainActivity obsahu

--- app/src/main/java/com/czechitast/janhanak/mojeprvniappka/MainActivity.java
+++ app/src/main/java/com/czechitast/janhanak/mojeprvniappka/MainActivity.java
@@ -13,25 +13,5 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Button login = findViewById(R.id.login);
-        login.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                //zmacknuNaTlacitko();
-                EditText myName = findViewById(R.id.my_name);
-                String name = myName.getText().toString();
-                startChat(name);
-            }
-        });
-    }
-
-    public void zmacknuNaTlacitko() {
-        //Toast.makeText(this, "Zmackla jsem na tlacitko", Toast.LENGTH_LONG).show();
-    }
-
-    public void startChat(String name) {
-        Intent intent = new Intent(this, ChatActivity.class);
-        intent.putExtra(Constants.USER_NAME,name);
-        startActivity(intent);
     }
 }
